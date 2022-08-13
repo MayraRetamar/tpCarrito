fd291aa8bf25a31b4149d902038b6c24c2b71d46
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-08-13 14:15:21 -0300

    correccion Correccion del código en ts

--- src/pagina2/index.ts
+++ src/pagina2/index.ts
@@ -4,6 +4,8 @@ let agregar = document.getElementById("btn-agregar");
 let calcular = document.getElementById("btn-calcular");
 let lista = document.getElementById("lista");
 let total = document.getElementById("total");
+let aplicar = document.getElementById("btn-aplicar");
+let descuento = document.getElementById("descuento");
 let productos: string[] = [];
 let precios: number[] = [];
 let suma: number = 0;
@@ -25,3 +27,14 @@ function calcularTotal() {
 }
 agregar?.addEventListener("click", agregarAlCarrito);
 calcular?.addEventListener("click", calcularTotal);
+
+function calcularDescuento() { 
+    if(suma > 2500) { 
+      descuento.innerHTML = ` Su compra supera los $2500 accede a un descuento del 10% total con descuento:  ${suma - suma * 0.1}`;
+
+    } else { 
+      descuento.innerHTML = `Con su compra de más de $2500 accede un descuento`;
+    }
+};
+
+aplicar?.addEventListener("click", calcularDescuento);
